a78bfd57ff7c495d04f079adc7d0c4036e4b3f0b
bawoori <kwangeun.kim@gmail.com>
2017-07-20 10:46:24 +0900

    ADD 액티비티 로직 개발 중

--- app/src/main/java/example/com/mycontact/AddContact.java
+++ app/src/main/java/example/com/mycontact/AddContact.java
@@ -2,12 +2,36 @@ package example.com.mycontact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AddContact extends AppCompatActivity {
+
+    private static final String TAG = "AddContact";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+
+        final EditText nameInput = (EditText) findViewById(R.id.editName);
+        final EditText codeInput = (EditText) findViewById(R.id.editCode);
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+
+       saveButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               String name = nameInput.getText().toString();
+               String code = codeInput.getText().toString();
+
+               Log.d(TAG, "name:" + name + " code:" + code);
+               finish();
+           }
+       });
+
+
     }
 }
--- app/src/main/java/example/com/mycontact/MainActivity.java
+++ app/src/main/java/example/com/mycontact/MainActivity.java
@@ -1,13 +1,58 @@
 package example.com.mycontact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView nameList = (ListView) findViewById(R.id.nameList);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        nameList.setAdapter(listAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume occured!!!");
+        refresh();
+    }
+
+    private void refresh() {
+        Log.d(TAG, "refresh occured!!!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.addmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.addButton:
+                Log.d(TAG, "addButton selected!!!!");
+                Intent i = new Intent(this, AddContact.class);
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
